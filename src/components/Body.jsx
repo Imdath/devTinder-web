14135f1dc6aff794887ac06bd0118c51ef32999e
Imdath <imdathmohamed@gmail.com>
2025-01-16 00:48:52 +0530

    redirect to login if not authenticated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import { BASE_URL } from '../utils/constants'
+import { addUser } from '../utils/userSlice'
+import { useDispatch } from 'react-redux'
+import axios from 'axios'
 
 const Body = () => {
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+
+	const fetchUser = async () => {
+		try {
+			const result = await axios.get(BASE_URL + '/profile/view', { withCredentials: true })
+			dispatch(addUser(result.data))
+		} catch (error) {
+			if (error.status === 401) {
+				navigate('/login')
+			}
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		fetchUser()
+	}, [])
+
 	return (
 		<>
 			<NavBar />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
 	const [theme, setTheme] = useState('cupcake') // Default theme
@@ -23,7 +24,9 @@ const NavBar = () => {
 	return (
 		<div className='navbar bg-base-300'>
 			<div className='flex-1'>
-				<a className='btn btn-ghost text-xl'>👩‍💻DevTinder</a>
+				<Link to='/' className='btn btn-ghost text-xl'>
+					👩‍💻DevTinder
+				</Link>
 			</div>
 
 			<div className='flex-none gap-2'>
@@ -50,14 +53,14 @@ const NavBar = () => {
 						<ul tabIndex={0} className='menu menu-sm dropdown-content bg-base-300 rounded-box z-[1] mt-3 w-52 p-2 shadow'>
 							<li>
 								<a className='no-underline pointer-events-none'>
-									Welcome, <span className='font-bold'>{user.firstName.toUpperCase()}</span>
+									Welcome, <span className='font-bold'>{user?.firstName?.toUpperCase()}</span>
 								</a>
 							</li>
 							<li>
-								<a className='justify-between'>
+								<Link to='/profile' className='justify-between'>
 									Profile
 									<span className='badge'>New</span>
-								</a>
+								</Link>
 							</li>
 							<li>
 								<a>Settings</a>
